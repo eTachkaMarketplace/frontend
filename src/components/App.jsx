c30211edd68174b885ab7913a88abdaee06ee61a
Yura <113457886+Yura-Platonov@users.noreply.github.com>
2023-10-16 20:40:32 +0300

    login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Login from "../components/Login/Login";
+import SignUp from "../components/SignUp/SignUp"; 
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <div>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            {/* <li>
+              <Link to="/reset">Reset</Link>
+            </li> */}
+            <li>
+              <Link to="/signup">SignUp</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/"  />
+        </Routes>
+      </div>
+    </Router>
   );
 };
+
+export default App;
