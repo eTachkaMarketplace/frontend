917df35838e3f11e132091ff22ac29d44de3f2e2
Yura <113457886+Yura-Platonov@users.noreply.github.com>
2023-11-03 00:48:58 +0200

    fix

--- src/components/Main/CarouselCar/CarouselCar.jsx
+++ src/components/Main/CarouselCar/CarouselCar.jsx
@@ -1,29 +1,18 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import { StyledSVG } from './CarouselCar.styled.jsx';
-import { FordSVG, HondaSVG, InfinitiSVG, KiaSVG, MazdaSVG,RenaultSVG,VolvoSVG } from './SVGCar';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { StyledFordSVG, StyledHondaSVG, StyledInfinitiSVG, StyledKiaSVG, StyledMazdaSVG,StyledRenaultSVG,StyledVolvoSVG } from './CarouselCar.styled.jsx';
 
 
 export const CarouselCar = () => (
-  <Carousel autoPlay>
+  <Carousel >
     <div>
-      <img alt="1" src="https://randomwordgenerator.com/img/picture-generator/52e3d2424355ac14f1dc8460962e33791c3ad6e04e507440762e79d09548c6_640.jpg" />
+    <StyledFordSVG />
+    <StyledHondaSVG />
+    <StyledInfinitiSVG />
+    <StyledKiaSVG />
+    <StyledMazdaSVG />
     </div>
-    <div>
-      <img alt="2" src="https://randomwordgenerator.com/img/picture-generator/57e3d6424351ad14f1dc8460962e33791c3ad6e04e507440772d7cdd9144c4_640.jpg" />
-      
-    </div>
-    <div>
-      <img alt="3" src="https://randomwordgenerator.com/img/picture-generator/54e7d2424352b10ff3d8992cc12c30771037dbf85254794175297ad4944d_640.jpg" />
-      
-    </div>
-    <div>
-      <img alt="4" src="https://randomwordgenerator.com/img/picture-generator/55e2d2454b54a414f1dc8460962e33791c3ad6e04e50744172277fd09345c7_640.jpg" />
-      
-    </div>
-    <div>
-      <img alt="5" src="https://randomwordgenerator.com/img/picture-generator/55e8d14b4952ab14f1dc8460962e33791c3ad6e04e5074417c2e7dd3974ec4_640.jpg" />
-      
-    </div>
-    </Carousel>
+    <StyledRenaultSVG />
+    <StyledVolvoSVG />
+  </Carousel>
 );
---
+++ src/components/Main/CarouselCar/CarouselCar.styled.jsx
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+import { ReactComponent as FordSVG } from '../../../images/SliderCar/Ford.svg';
+import { ReactComponent as HondaSVG } from '../../../images/SliderCar/Honda.svg';
+import { ReactComponent as InfinitiSVG } from '../../../images/SliderCar/Infiniti.svg';
+import { ReactComponent as KiaSVG } from '../../../images/SliderCar/Kia.svg';
+import { ReactComponent as MazdaSVG } from '../../../images/SliderCar/Mazda.svg';
+import { ReactComponent as RenaultSVG } from '../../../images/SliderCar/Renault.svg';
+import { ReactComponent as VolvoSVG } from '../../../images/SliderCar/Volvo.svg';
+
+export const StyledFordSVG = styled(FordSVG)`
+  transition: fill 0.3s;
+
+  &:hover, &:focus {
+    fill: blue;
+  }
+`;
+
+export const StyledHondaSVG = styled(HondaSVG)`
+  transition: fill 0.3s;
+
+  &:hover, &:focus {
+    fill: blue;
+  }
+`;
+
+export const StyledInfinitiSVG = styled(InfinitiSVG)`
+  transition: fill 0.3s;
+
+  &:hover, &:focus {
+    fill: blue;
+  }
+`;
+
+export const StyledKiaSVG = styled(KiaSVG)`
+  transition: fill 0.3s;
+
+  &:hover, &:focus {
+    fill: blue;
+  }
+`;
+
+export const StyledMazdaSVG = styled(MazdaSVG)`
+  transition: fill 0.3s;
+
+  &:hover, &:focus {
+    fill: blue;
+  }
+`;
+
+export const StyledRenaultSVG = styled(RenaultSVG)`
+  transition: fill 0.3s;
+
+  &:hover, &:focus {
+    fill: blue;
+  }
+`;
+
+export const StyledVolvoSVG = styled(VolvoSVG)`
+  transition: fill 0.3s;
+
+  &:hover, &:focus {
+    fill: blue;
+  }
+`;
