1337d939d729c537b40b425fc4995a4b829ae99d
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-11-08 02:27:45 +0200

    pagination

--- src/components/Main/New/NewCars.jsx
+++ src/components/Main/New/NewCars.jsx
@@ -1,69 +1,109 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCarsThunk } from 'redux/cars/operations';
 import { selectCars } from 'redux/cars/selectors';
 import { SectionCar } from '../Popular/Popular.styled';
 import { heartSvg1 } from '../Popular/PopularSvg';
-
+import { PagDiv } from './NewCars.styled';
+import { leftArrow, rightArrow } from './NewCarsSvg';
 
 const NewCars = () => {
   const dispatch = useDispatch();
-  const page = 1;
-  const limit = 6;
+  const [page, setPage] = useState(1);
   const cars = useSelector(selectCars);
+
+  const limit = 6;
+  const totalPages = 6;
 
   useEffect(() => {
     dispatch(getCarsThunk({ limit, page }));
-  }, [dispatch]);
+  }, [dispatch, page]);
+
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+  const startPage = Math.max(1, page - 2);
+  const endPage = Math.min(totalPages, startPage + 4);
+
+  const visiblePages = Array.from(
+    { length: 5 },
+    (_, index) => endPage - 4 + index
+  ).filter(num => num > 0 && num <= totalPages);
 
   return (
-    
-      <pagDiv>
-        <SectionCar>
-          <h2 className="carTitle">Нові оголошення</h2>
-          <ul className="carList">
-            {cars
-              ? cars.map(car => {
-                  const addressParts = car.address.split(',');
-                  const City = addressParts[1] ? addressParts[1].trim() : '';
-                  return (
-                    <li className="carItem" key={car.id}>
-                      <img className="imgCar" src={car.img} alt="Car view" />
-                      <h3 className="blackTitle ">
-                        {car.make} {car.model} {car.year}
-                      </h3>
-                      <ul className="carDescrList">
-                        <li>
-                          <p className="carDescrPrice">{car.rentalPrice}</p>
-                        </li>
-                        <li className="cityRight">
-                          <p className="carDescrCity">{City}</p>
-                        </li>
-                      </ul>
+    <PagDiv>
+      <SectionCar>
+        <h2 className="carTitle">Нові оголошення</h2>
+        <ul className="carList">
+          {cars
+            ? cars.map(car => {
+                const addressParts = car.address.split(',');
+                const City = addressParts[1] ? addressParts[1].trim() : '';
+                return (
+                  <li className="carItem" key={car.id}>
+                    <img className="imgCar" src={car.img} alt="Car view" />
+                    <h3 className="blackTitle ">
+                      {car.make} {car.model} {car.year}
+                    </h3>
+                    <ul className="carDescrList">
+                      <li>
+                        <p className="carDescrPrice">{car.rentalPrice}</p>
+                      </li>
+                      <li className="cityRight">
+                        <p className="carDescrCity">{City}</p>
+                      </li>
+                    </ul>
 
-                      <button
-                        // onClick={() => {
-                        //   if (isCarInFav(car.id)) {
-                        //     removeFromFav(car.id);
-                        //   } else {
-                        //     addFavorite(car.id);
-                        //   }
-                        // }}
-                        className="svg"
-                        type="button"
-                      >
-                        {heartSvg1}
-                        {/* {isCarInFav(car.id) ? heartSvg2 : heartSvg1}  */}
-                      </button>
-                    </li>
-                  );
-                })
-              : null}
-          </ul>
+                    <button
+                      // onClick={() => {
+                      //   if (isCarInFav(car.id)) {
+                      //     removeFromFav(car.id);
+                      //   } else {
+                      //     addFavorite(car.id);
+                      //   }
+                      // }}
+                      className="svg"
+                      type="button"
+                    >
+                      {heartSvg1}
+                      {/* {isCarInFav(car.id) ? heartSvg2 : heartSvg1}  */}
+                    </button>
+                  </li>
+                );
+              })
+            : null}
+        </ul>
       </SectionCar>
-      <div></div>
-      </pagDiv>
-    
+      <div className="paginDiv">
+        <button
+          className={`pagination-button ${page === 1 ? 'hidden' : ''}`}
+          onClick={prevPage}
+        >
+          {leftArrow}
+        </button>
+        {visiblePages.map(pageNum => (
+          <button
+            key={pageNum}
+            className={`pagination-button ${page === pageNum ? 'active' : ''}`}
+            onClick={() => setPage(pageNum)}
+          >
+            {pageNum}
+          </button>
+        ))}
+        <button
+          className={`pagination-button ${page === totalPages ? 'hidden' : ''}`}
+          onClick={nextPage}
+        >
+          {rightArrow}
+        </button>
+      </div>
+    </PagDiv>
   );
 };
 export default NewCars;
---
+++ src/components/Main/New/NewCars.styled.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+
+export const PagDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .paginDiv {
+    display: flex;
+    align-items: center;
+    height: 40px;
+    gap: 50px;
+    margin-top: 80px;
+    margin-bottom: 125px;
+  }
+
+  .pagination-button {
+    display: flex;
+    border: 0;
+    background-color: transparent;
+    color: #b0a9a9;
+    font-family: 'Roboto', sans-serif;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+  .active {
+    color: #10187e;
+  }
+  .hidden {
+    display: none;
+  }
+`;
