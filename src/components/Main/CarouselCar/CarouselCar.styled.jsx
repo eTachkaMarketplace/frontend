1df6fc9ff087a602473df62dd3d8c12107c57036
Yura <yuraplatonov2000@gmail.com>
2023-11-08 17:37:09 +0200

    fix styles

--- src/components/Main/CarouselCar/CarouselCar.styled.jsx
+++ src/components/Main/CarouselCar/CarouselCar.styled.jsx
@@ -6,10 +6,20 @@ import { ReactComponent as KiaSVG } from '../../../images/SliderCar/Kia.svg';
 import { ReactComponent as MazdaSVG } from '../../../images/SliderCar/Mazda.svg';
 import { ReactComponent as RenaultSVG } from '../../../images/SliderCar/Renault.svg';
 import { ReactComponent as VolvoSVG } from '../../../images/SliderCar/Volvo.svg';
-// import { ReactComponent as ArrowSVG } from '../../../images/SliderCar/ArrowRighrSVG.svg';
+// import { ReactComponent as Next } from '../../../images/SliderCar/SplideNext.svg';
+// import { ReactComponent as Prev } from '../../../images/SliderCar/SplidePrev.svg';
 
-// export const StyledArrowSVG = styled(ArrowSVG)`
-//   fill: black;
+// export const StyledNext = styled(Next)`
+//   fill: red;
+//   transition: fill 0.3s;
+
+//   &:hover, &:focus {
+//     fill: blue;
+//   }
+// `;
+
+// export const StyledPrev = styled(Prev)`
+//   fill: red;
 //   transition: fill 0.3s;
 
 //   &:hover, &:focus {
@@ -19,70 +29,41 @@ import { ReactComponent as VolvoSVG } from '../../../images/SliderCar/Volvo.svg'
 
 export const StyledWrapper = styled.p`
   color:  inherit;
-  font-family: Inter;
+  font-family: 'Roboto',sans-serif;
   font-size: 18px;
-  /* padding-left: 15px; */
-
-  /* &:hover, &:focus {
-    color: blue;
-  } */
-
-
 `;
 
 export const StyledFordSVG = styled(FordSVG)`
   transition: fill 0.3s;
-
-  /* &:hover, &:focus {
-    fill: blue;
-  } */
 `;
 
 export const StyledHondaSVG = styled(HondaSVG)`
   transition: fill 0.3s;
 
-  /* &:hover, &:focus {
-    fill: blue;
-  } */
 `;
 
 export const StyledInfinitiSVG = styled(InfinitiSVG)`
   transition: fill 0.3s;
 
-  /* &:hover, &:focus {
-    fill: blue;
-  } */
 `;
 
 export const StyledKiaSVG = styled(KiaSVG)`
   transition: fill 0.3s;
 
-  /* &:hover, &:focus {
-    fill: blue;
-  } */
 `;
 
 export const StyledMazdaSVG = styled(MazdaSVG)`
   transition: fill 0.3s;
 
-  /* &:hover, &:focus {
-    fill: blue;
-  } */
 `;
 
 export const StyledRenaultSVG = styled(RenaultSVG)`
   transition: fill 0.3s;
 
-  /* &:hover, &:focus {
-    fill: blue;
-  } */
 `;
 
 export const StyledVolvoSVG = styled(VolvoSVG)`
   transition: fill 0.3s;
 
-  /* &:hover, &:focus {
-    fill: blue;
-  } */
 `;
 
