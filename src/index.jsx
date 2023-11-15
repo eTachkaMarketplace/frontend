5cb26cea40253b0063384b16b25774df5ae92139
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-11-15 15:40:22 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
