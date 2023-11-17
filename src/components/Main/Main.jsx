5744986af24f6708ecb63b2151fb0b532caee60e
Yura <113457886+Yura-Platonov@users.noreply.github.com>
2023-11-17 20:52:31 +0200

    delete main page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,8 @@ import HeroSection from './HeroSection/HeroSection';
 import { CarouselCar } from './CarouselCar/CarouselCar';
 import Catalog from './Popular/Popular';
 import NewCars from './New/NewCars';
+import AccPage from '../../pages/AccountPage/AccountPage';
+
 
 const Main = () => {
   return (
@@ -12,8 +14,9 @@ const Main = () => {
       <CarouselCar />
       <Catalog />
       <NewCars/>
+      <AccPage/>
     </MainWrapper>
-  );
+    );
 };
 
 export default Main;
