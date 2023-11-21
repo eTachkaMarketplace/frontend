b9b3a456f24fc9b04762b4c24e3797f114575ee0
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-11-21 05:18:31 +0200

    add

---
+++ src/components/SearchForm/SearchForm.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Search = styled.div`
+  display: flex;
+  width: 412px;
+  height: 1286px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
+  flex-shrink: 0;
+
+  border-radius: 8px;
+  background:  #f1f1f1;
+`;
---
+++ src/pages/SearchPage/SearchPage.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+
+export const Wraper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`
