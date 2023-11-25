ea96bad5a53f424898ea99e7c9125d0083467a71
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-11-25 13:21:48 +0200

    topOfForm

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,91 @@
+import { Field, Formik, Form } from 'formik';
 import { Search } from "./SearchForm.styled";
 
-
 export const SearchForm = () => {
-    return <Search>
-      
-  </Search>;
+   const initialValues = {
+     status: '',
+     brand: '',
+     model: '',
+     region: '',
+     yearFrom: '',
+     yearTo: '',
+     priceFrom: '',
+     priceTo: '',
+  };
+  const handleSearch = values => {
+    console.log(values)
+  };
+  return (
+    <Search>
+      <Formik initialValues={initialValues} onSubmit={handleSearch}>
+        <Form>
+          <div className="topWraper">
+            <h3 className="title">Пошук</h3>
+            <label>
+              <Field className="fieldLong marg16" as="select" name="status">
+                <option value="">Статус</option>
+                <option value="нові">Нові</option>
+                <option value="вживані">Вживані</option>
+              </Field>
+            </label>
+            <label>
+              <Field className="fieldLong marg16" as="select" name="brand">
+                <option value="">Марка</option>
+                <option value="BMW">BMW</option>
+                <option value="Mersedes">Mersedes</option>
+              </Field>
+            </label>
+            <label>
+              <Field className="fieldLong marg16" as="select" name="model">
+                <option value="">Модель</option>
+                <option value="Щось1">Щось1</option>
+                <option value="Щось2">Щось2</option>
+              </Field>
+            </label>
+            <label>
+              <Field className="fieldLong marg16" as="select" name="region">
+                <option value="">Область</option>
+                <option value="Щось1">Щось1</option>
+                <option value="Щось2">Щось2</option>
+              </Field>
+            </label>
+            <div className="shortDiv marg16">
+              <label>
+                <Field className="fieldShort" as="select" name="yearFrom">
+                  <option value="">Рік від</option>
+                  <option value="Щось1">Щось1</option>
+                  <option value="Щось2">Щось2</option>
+                </Field>
+              </label>
+              <label>
+                <Field className="fieldShort" as="select" name="yearTo">
+                  <option value="">Рік до</option>
+                  <option value="Щось1">Щось1</option>
+                  <option value="Щось2">Щось2</option>
+                </Field>
+              </label>
+            </div>
+            <div className="shortDiv">
+              <label>
+                <Field className="fieldShort" as="select" name="priceFrom">
+                  <option value="">Ціна від</option>
+                  <option value="Щось1">Щось1</option>
+                  <option value="Щось2">Щось2</option>
+                </Field>
+              </label>
+              <label>
+                <Field className="fieldShort" as="select" name="priceTo">
+                  <option value="">Ціна до</option>
+                  <option value="Щось1">Щось1</option>
+                  <option value="Щось2">Щось2</option>
+                </Field>
+              </label>
+            </div>
+          </div>
+          <button type="submit">Пошук</button>
+        </Form>
+      </Formik>
+    </Search>
+  );
 };
 export default SearchForm;
--- src/components/SearchForm/SearchForm.styled.js
+++ src/components/SearchForm/SearchForm.styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Search = styled.div`
   display: flex;
@@ -6,9 +6,80 @@ export const Search = styled.div`
   height: 1286px;
   flex-direction: column;
   align-items: flex-start;
-  gap: 32px;
   flex-shrink: 0;
 
   border-radius: 8px;
-  background:  #f1f1f1;
+  background: #f1f1f1;
+
+  select {
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    cursor: pointer;
+  }
+  label {
+    display: flex;
+    position: relative;
+  }
+
+  label:after {
+    content: '▼';
+    position: absolute;
+    top: 20px;
+    right: 16px;
+    pointer-events: none;
+  }
+
+
+  .title {
+    margin-bottom: 24px;
+    width: 372px;
+
+    color: #000;
+
+    font-family: 'Roboto', sans-serif;
+    font-size: 26px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
+  .topWraper {
+    display: flex;
+    flex-direction: column;
+    margin-left: 20px;
+    margin-top: 16px;
+  }
+  .fieldLong {
+    display: flex;
+
+    width: 380px;
+    height: 56px;
+    padding: 4px 16px;
+    justify-content: space-between;
+    align-items: center;
+
+    border-radius: 8px;
+    background: #fff;
+  }
+
+  .fieldShort {
+    display: flex;
+
+    width: 183px;
+    height: 56px;
+    padding: 4px 16px;
+    justify-content: space-between;
+    align-items: center;
+
+    border-radius: 8px;
+    background: #fff;
+  }
+
+  .shortDiv {
+    display: flex;
+    gap: 14px;
+  }
+  .marg16 {
+    margin-bottom: 16px;
+  }
 `;
