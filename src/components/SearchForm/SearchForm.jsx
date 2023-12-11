51d2470a520a03574b3e9c5673b9cf12c348f1c2
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-12-11 10:40:34 +0200

    edits from

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,18 +5,18 @@ import { useDispatch } from 'react-redux';
 import { setIsOpen } from 'redux/modal/modalSlice';
 
 export const SearchForm = ({ initialValues, onSubmit }) => {
-    const distatch = useDispatch();
+  const distatch = useDispatch();
 
   const formikRef = useRef(null);
-   useEffect(() => {
-     if (formikRef.current) {
-       formikRef.current.resetForm({ values: initialValues });
-     }
-   }, [initialValues]);
-  
+  useEffect(() => {
+    if (formikRef.current) {
+      formikRef.current.resetForm({ values: initialValues });
+    }
+  }, [initialValues]);
+
   const clearForm = () => {
     distatch(setIsOpen(true));
-  }
+  };
   return (
     <Search>
       <Formik
@@ -30,8 +30,9 @@ export const SearchForm = ({ initialValues, onSubmit }) => {
             <label>
               <Field className="fieldLong marg16" as="select" name="status">
                 <option value="">Статус</option>
-                <option value="Нові">Нові</option>
-                <option value="Вживані">Вживані</option>
+                <option value="New">Нові</option>
+                <option value="Used">Вживані</option>
+                <option value="Servitude">Під пригон</option>
               </Field>
             </label>
             <label>
@@ -93,15 +94,20 @@ export const SearchForm = ({ initialValues, onSubmit }) => {
             <label>
               <Field className="fieldLong marg16" as="select" name="bodyType">
                 <option value="">Тип кузова</option>
-                <option value="нові">Тип1</option>
-                <option value="вживані">Тип2</option>
+                <option value="Universal">Універсал</option>
+                <option value="Sedan">Седан</option>
+                <option value="Cabriolet">Кабріолет</option>
+                <option value="Compartment">Купе</option>
               </Field>
             </label>
             <label>
               <Field className="fieldLong marg16" as="select" name="fuelType">
-                <option value="">Палево</option>
-                <option value="BMW">Бензин</option>
-                <option value="Mersedes">Дизель</option>
+                <option value="">Двигун</option>
+                <option value="Electro">Електрична силова установка</option>
+                <option value="Hibrid">Гібрід</option>
+                <option value="Gasoline">Бензиновий</option>
+                <option value="Gaseous">Газовий</option>
+                <option value="Diesel">Дизельний</option>
               </Field>
             </label>
             <label>
@@ -111,35 +117,12 @@ export const SearchForm = ({ initialValues, onSubmit }) => {
                 name="engineCapacity"
               >
                 <option value="">Об'єм двигуна</option>
-                <option value="Щось1">Щось1</option>
-                <option value="Щось2">Щось2</option>
+                <option value="Microliter">До 1,1 літра</option>
+                <option value="Low-volume">Від 1,2 до 1,7 літра</option>
+                <option value="Medium-sized">Від 1,8 до 3,3 літра</option>
+                <option value="Large-capacity">Від 3,5 літра</option>
               </Field>
             </label>
-            {/* <div className="shortDiv marg16">
-              <label>
-                <Field
-                  className="fieldShort "
-                  as="select"
-                  name="engineCapacityFrom"
-                >
-                  <option value="">Об'єм від</option>
-                  <option value="Щось1">Щось1</option>
-                  <option value="Щось2">Щось2</option>
-                </Field>
-              </label>
-
-              <label>
-                <Field
-                  className="fieldShort "
-                  as="select"
-                  name="engineCapacityFromTo"
-                >
-                  <option value="">Об'єм до</option>
-                  <option value="Щось1">Щось1</option>
-                  <option value="Щось2">Щось2</option>
-                </Field>
-              </label>
-            </div> */}
             <div className="shortDiv marg16">
               <label>
                 <Field className="fieldShort " as="select" name="mileageFrom">
@@ -160,8 +143,9 @@ export const SearchForm = ({ initialValues, onSubmit }) => {
             <label>
               <Field className="fieldLong marg16" as="select" name="driveType">
                 <option value="">Привід</option>
-                <option value="Щось1">Щось1</option>
-                <option value="Щось2">Щось2</option>
+                <option value="Front">Передній</option>
+                <option value="Posterior">Задній</option>
+                <option value="Full">Повний</option>
               </Field>
             </label>
 
@@ -172,22 +156,51 @@ export const SearchForm = ({ initialValues, onSubmit }) => {
                 name="transmission"
               >
                 <option value="">Коробка передач</option>
-                <option value="Щось1">Автомат</option>
-                <option value="Щось2">Ручна</option>
+                <option value="Mechanical">Механічна</option>
+                <option value="Automatic">Автоматична</option>
+                <option value="Robotic">Роботизована</option>
+                <option value="Variable">Варіативна</option>
               </Field>
             </label>
             <label>
               <Field className="fieldLong marg16" as="select" name="condition">
                 <option value="">Технічний стан</option>
-                <option value="Щось1">Щось1</option>
-                <option value="Щось2">Щось2</option>
+                <option
+                  value="Completely intact
+"
+                >
+                  Повністю непошкоджене
+                </option>
+                <option value="Professionally repaired damage">
+                  Професійно відремонтовані пошкодження
+                </option>
+                <option value="Unrepaired damage">
+                  Не відремонтовані пошкодження
+                </option>
+                <option value="Not running/For spare parts">
+                  Не на ходу/На запчастини
+                </option>
               </Field>
             </label>
             <label>
               <Field className="fieldLong" as="select" name="color">
                 <option value="">Колір</option>
-                <option value="Щось1">Щось1</option>
-                <option value="Щось2">Щось2</option>
+                <option value="White">Білий</option>
+                <option value="Black">Чорний</option>
+                <option value="Gray">Сірий</option>
+                <option value="Red">Червоний</option>
+                <option value="Blue">Синій</option>
+                <option value="Pink">Рожевий</option>
+                <option value="Green">Зелений</option>
+                <option value="Orange">Помаранчовий</option>
+                <option value="Burgundy">Бордовий</option>
+                <option value="Brown">Коричневий</option>
+                <option value="Yellow">Жовтий</option>
+                <option value="Violet">Фіолетовий</option>
+                <option value="Gold">Золотий</option>
+                <option value="Silver">Срібний</option>
+                <option value="beige">бежевий</option>
+                <option value="Multicolor">Мультиколір</option>
               </Field>
             </label>
           </div>
