0f5db9c2a16f1d1050d4559a98aa6f2b4e8e93e1
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2024-01-04 06:50:57 +0200

    pass start

---
+++ src/pages/ChangePassPage/ChangePass.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+
+export default function ChangePass() {
+    const { id } = useParams();
+    useEffect(() => {
+
+        
+     },[])
+  return (
+    <></>
+  );
+}
