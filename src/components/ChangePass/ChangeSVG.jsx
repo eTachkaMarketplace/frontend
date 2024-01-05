f623badcc4a9e7cbb45ddc834b8e91367e34fc65
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2024-01-05 19:40:00 +0200

    add for usser

---
+++ src/components/ChangePass/ChangeSVG.jsx
@@ -0,0 +1,20 @@
+export const SvgTreu = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="45"
+        height="45"
+        viewBox="0 0 45 45"
+        fill="none"
+      >
+        <rect width="45" height="45" rx="22.5" fill="#069024" />
+        <path
+          d="M7.5 22.4999L16.7808 31.7807L36.6698 11.8936"
+          stroke="white"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    );
+}
--- src/components/ForgotPass/ForgotPass.jsx
+++ src/components/ForgotPass/ForgotPass.jsx
@@ -6,6 +6,7 @@ import { ErrorSVG } from 'components/LoginForm/chackBox';
 import { useEffect, useState } from 'react';
 import { selectError } from 'redux/user/selectors';
 import { forgotPass } from 'redux/user/opetations';
+import { NavLink } from 'react-router-dom';
 
 const userSchema = Yup.object().shape({
   email: Yup.string()
@@ -20,12 +21,14 @@ const userSchema = Yup.object().shape({
 export default function ForgotPass() {
   const dispatch = useDispatch();
   const [refError, setRefError] = useState(false);
+  const [chacked, setChacked] = useState(false);
   const requestError = useSelector(selectError);
 
   const handleSubmit = (values, { resetForm, setSubmitting }) => {
     const { email } = values;
     try {
       dispatch(forgotPass({ email }));
+      setChacked(true);
       resetForm();
       setSubmitting(false);
     } catch (error) {
@@ -38,43 +41,58 @@ export default function ForgotPass() {
   }, [requestError]);
 
   return (
-    <Formik
-      onSubmit={handleSubmit}
-      initialValues={{ email: '' }}
-      validationSchema={userSchema}
-    >
-      {values => {
-        return (
-          <Wraper>
-            <h4 className="title">Відновлення паролю</h4>
-            <p className="text">
-              Щоб відновити пароль, введіть Ваш e-mail, який Ви вказували при
-              реєстрації
-            </p>
-            <Form>
-              <Label className={` marg24`}>
-                <Input>
-                  <Field
-                    className={refError ? 'is-invalid' : ''}
-                    type="email"
-                    name="email"
-                    placeholder="Введіть електрону пошту"
-                    value={values.email}
-                  />
+    <>
+      {chacked ? (
+        <Wraper>
+          <h4 className="title">Відновлення паролю</h4>
+          <p className="text">
+            На Ваш e-mail user@gmail.com протягом хвилини прийде лист з
+            посиланням для відновлення паролю.{' '}
+            <span className='textBig'>Перейдіть за посиланням у листі.</span>
+          </p>
+          <button className='mainBtn' type="button" onClick={()=>{setChacked(false)}}>Надіслати на інший e-mail </button>
+          <NavLink className='link' to='/'>На головну</NavLink>
+        </Wraper>
+      ) : (
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={{ email: '' }}
+          validationSchema={userSchema}
+        >
+          {values => {
+            return (
+              <Wraper>
+                <h4 className="title">Відновлення паролю</h4>
+                <p className="text">
+                  Щоб відновити пароль, введіть Ваш e-mail, який Ви вказували
+                  при реєстрації
+                </p>
+                <Form>
+                  <Label className={` marg24`}>
+                    <Input>
+                      <Field
+                        className={refError ? 'is-invalid' : ''}
+                        type="email"
+                        name="email"
+                        placeholder="Введіть електрону пошту"
+                        value={values.email}
+                      />
 
-                  {refError ? (
-                    <span className="errorSVGemail">
-                      <ErrorSVG />
-                    </span>
-                  ) : null}
-                </Input>
-                <ErrorMessage name="email" component="div" />
-              </Label>
-              <Button type="submit">Відновити</Button>
-            </Form>
-          </Wraper>
-        );
-      }}
-    </Formik>
+                      {refError ? (
+                        <span className="errorSVGemail">
+                          <ErrorSVG />
+                        </span>
+                      ) : null}
+                    </Input>
+                    <ErrorMessage name="email" component="div" />
+                  </Label>
+                  <Button type="submit">Відновити</Button>
+                </Form>
+              </Wraper>
+            );
+          }}
+        </Formik>
+      )}
+    </>
   );
 }
