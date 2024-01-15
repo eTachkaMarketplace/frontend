6c027704fccb753c193b905fd1349e475007c163
Shorinami <shorinami@atombuilt.com>
2024-01-15 19:13:16 +0200

    fix: Advertisement card in home page is not properly sized

--- src/components/HomePage/AdvertisementCard.jsx
+++ src/components/HomePage/AdvertisementCard.jsx
@@ -1,29 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function AdvertisementCard({ data }) {
   const { car } = data;
 
   return (
-    <div
+    <Link
+      to={`/AdvertisementByID/${data.id}`}
       className="flex flex-col flex-1 p-4 gap-3 cursor-pointer bg-blue-50 border-2 border-transparent
     hover:border-blue-200 rounded transition duration-300 "
     >
-      <Link to={`/AdvertisementByID/${data.id}`}>
-        <img
-          className={'rounded-lg aspect-preview w-full h-full object-cover'}
-          src={data.previewImage}
-          alt={'Advertisement Preview'}
-        />
-        <div>
-          <h4 className={'text-lg font-semibold'}>
-            {car.brand} {car.model} {car.year}
-          </h4>
-          <div className={'flex flex-row justify-between items-center'}>
-            <p>$ {car.price}</p>
-            <p className={'text-sm'}>{data.region}</p>
-          </div>
+      <img
+        className={'rounded-lg aspect-preview w-full h-full object-cover'}
+        src={data.previewImage}
+        alt={'Advertisement Preview'}
+      />
+      <div>
+        <h4 className={'text-lg font-semibold'}>
+          {car.brand} {car.model} {car.year}
+        </h4>
+        <div className={'flex flex-row justify-between items-center'}>
+          <p>$ {car.price}</p>
+          <p className={'text-sm'}>{data.region}</p>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
