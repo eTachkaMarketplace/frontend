542e90261f339cdfd9701751681190bd2c44ca94
Yura <yuraplatonov2000@gmail.com>
2024-02-06 15:13:36 +0200

    try

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,8 +15,7 @@ const Header = () => {
   useEffect(() => {
     dispatch(getAdvFav());
   }, [dispatch]);
-
-  
+ 
 
   return (
     <Container>
@@ -66,3 +65,76 @@ const Header = () => {
 };
 
 export default Header;
+
+
+// import React, { useEffect, useState } from 'react';
+// import { Container, HeaderWrapper, Navigation, User, StyledSpan, StyledLink, UserIcon, ButtonSale, StyledUserSVG, StyledHeartSVG, StyledAddSVG, ButtonText } from './Header.styled';
+// import { NavLink } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { getAdvFav } from 'redux/advertisment/operations';
+// import { selectIsLoggedIn } from '../../redux/auth/selectors';
+// import Logo from '../../images/Header/LOGO.png';
+// import { selectAdverstisementsFavorite } from 'redux/advertisment/selectors';
+
+// const Header = () => {
+//   const dispatch = useDispatch();
+//   const favoritesFromState = useSelector(selectAdverstisementsFavorite);
+//   const isLoggedIn = useSelector(selectIsLoggedIn);
+//   const [favoritesCount, setFavoritesCount] = useState(0);
+
+//   useEffect(() => {
+//     dispatch(getAdvFav());
+//   }, [dispatch]);
+
+//   useEffect(() => {
+//     setFavoritesCount(favoritesFromState.length);
+//   }, [favoritesFromState.length]);
+
+//   return (
+//     <Container>
+//       <HeaderWrapper>
+//         <NavLink to={'/'}>
+//           <img src={Logo} alt="Logo" />
+//         </NavLink>
+//         <Navigation>
+//           <ul>
+//             <li>
+//               <StyledLink to="/search?category=Нові">Нові</StyledLink>
+//             </li>
+//             <li>
+//               <StyledLink to="/search?category=Вживані">Вживані</StyledLink>
+//             </li>
+//             <li>
+//               <StyledLink to="/search?category=Під пригон">Під пригон</StyledLink>
+//             </li>
+//           </ul>
+//         </Navigation>
+//         <User>
+//           <NavLink to="/advertisementPage">
+//             <ButtonSale>
+//               <StyledAddSVG />
+//               <ButtonText>Продати</ButtonText>
+//             </ButtonSale>
+//           </NavLink>
+//           <UserIcon>
+//             <NavLink to="/account?favourites=true">
+//               <div style={{ position: 'relative' }}>
+//                 <StyledHeartSVG />
+//                 {favoritesCount > 0 && (
+//                   <StyledSpan>
+//                     {favoritesCount}
+//                   </StyledSpan>
+//                 )}
+//               </div>
+//             </NavLink>
+//             <NavLink to={isLoggedIn ? '/account' : '/authorization'}>
+//               <StyledUserSVG />
+//             </NavLink>
+//           </UserIcon>
+//         </User>
+//       </HeaderWrapper>
+//     </Container>
+//   );
+// };
+
+// export default Header;
