097fa85fbb1e425a7a3f4f53e3d83a0fd2f532dc
Yura <yuraplatonov2000@gmail.com>
2024-02-16 00:33:27 +0200

    privacy policy page done

--- src/pages/PrivacyPolicyPage/PrivacyPolicyPage.styled.jsx
+++ src/pages/PrivacyPolicyPage/PrivacyPolicyPage.styled.jsx
@@ -9,13 +9,13 @@ export const Title = styled.h1`
   color: rgb(20, 20, 20);
   font-size: 18px;
   font-weight: 500;
-  margin-bottom: 30px;
 `;
 
 export const SubTitle = styled.h2`
   color: rgb(20, 20, 20);
   font-size: 16px;
   font-weight: 500;
+  margin-top: 30px;
   margin-bottom: 15px;
 `;
 
@@ -24,9 +24,47 @@ export const Content = styled.div`
     font-family: 'Roboto', sans-serif;
     font-size: 14px;
     font-weight: 400;
+    }
+
+    strong{
+      font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    }
+    
+  li  {
+      font-family: 'Roboto', sans-serif;
+      font-size: 14px;
+      font-weight: 400;
+      }
+
+  a {
+      text-decoration: none; 
   }
 
-  strong {
-    font-weight: bold;
+  a:hover {
+      color: #007bff; 
+      text-decoration: underline;
   }
+
+  .list_ol {
+    list-style-type: none; 
+      counter-reset: item;
+      display: inline-block; 
+      padding: 0; 
+    }
+    
+
+  .list_ol li:before {
+        content: counter(item) ")"; 
+        counter-increment: item;
+        margin-right: 5px; 
+    }
+
+
+.list_ul {
+            list-style: inside;
+            padding: 0; 
+          }
+
 `;
