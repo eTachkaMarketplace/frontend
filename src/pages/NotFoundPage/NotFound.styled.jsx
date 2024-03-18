6c4f897a29a76594cd71113b37cd5bd8ac38c313
Yura <yuraplatonov2000@gmail.com>
2024-03-18 22:38:57 +0200

    add mob 404

--- src/pages/NotFoundPage/NotFound.styled.jsx
+++ src/pages/NotFoundPage/NotFound.styled.jsx
@@ -1,21 +1,38 @@
 import styled from 'styled-components';
+import { MOB, TAB, DESK1, DESK2 } from '../../components/constants';
 
 export const Container = styled.div`
-  position: relative;
+  /* position: relative; */
 
-  display: flex;
+  /* display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: center; */
 `;
 
 export const ErrorContainer = styled.div`
-  margin-top: 80px;
-  margin-bottom: 134px;
   background-color: transparent;
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  ${MOB} {
+    padding:60px 30px 80px 30px;
+  }
+
+  ${TAB} {
+    padding: 60px 60px 80px 60px;
+  }
+
+  ${DESK1} {
+    margin-top: 60px;
+    margin-bottom: 110px;
+  }
+
+  ${DESK2} {
+    margin-top: 80px;
+    margin-bottom: 134px;
+  }
 
   .cansel2 {
     margin-top: 20px;
@@ -24,37 +41,56 @@ export const ErrorContainer = styled.div`
     background-color: transparent;
     text-align: center;
     font-family: 'Roboto', sans-serif;
-    font-size: 22px;
     font-style: normal;
     font-weight: 500;
     line-height: normal;
 
     display: flex;
-    width: 454px;
-    height: 54px;
-    padding: 14px 24px 14px 24px;
+   
     justify-content: center;
     align-items: center;
     gap: 4px;
 
     border-radius: 8px;
     border: 2px solid #0040bd;
-  }
-  .confirm2 {
-    margin-top: 61px;
+
+    ${MOB} {
+      font-size: 18px;
+    width: 330px;
+    height: 48px;
+    padding: 10px 20px;
+
+    }
+
+  ${TAB} {
+    font-size: 20px;
+    width: 330px;
+    height: 48px;
+    padding: 10px 20px;
     
+  }
+
+  ${DESK1} {
+    font-size: 22px;
+
+    width: 454px;
+    height: 54px;
+    padding: 14px 24px 14px 24px;
+  }
+
+  ${DESK2} {
+ 
+  }
+  }
+  .confirm2 {    
     color: #fff;
     text-align: center;
     font-family: 'Roboto', sans-serif;
-    font-size: 22px;
     font-style: normal;
     font-weight: 500;
     line-height: normal;
 
     display: flex;
-    width: 454px;
-    height: 54px;
-    padding: 14px 24px 14px 24px;
     justify-content: center;
     align-items: center;
     gap: 4px;
@@ -64,6 +100,36 @@ export const ErrorContainer = styled.div`
     &:hover {
       background: linear-gradient(160deg, #0040bd 19.1%, #195b01 78.89%);
     }
+
+    ${MOB} {
+      font-size: 18px;
+      margin-top: 40px;
+    width: 330px;
+    height: 48px;
+    padding: 10px 20px;
+
+    }
+
+  ${TAB} {
+    font-size: 20px;
+    margin-top: 40px;
+    width: 330px;
+    height: 48px;
+    padding: 10px 20px;
+    
+  }
+
+  ${DESK1} {
+    font-size: 22px;
+    margin-top: 61px;
+    width: 454px;
+    height: 54px;
+    padding: 14px 24px 14px 24px;
+  }
+
+  ${DESK2} {
+ 
+  }
   }
 `;
 export const Title = styled.h2`
@@ -74,16 +140,31 @@ export const Title = styled.h2`
   font-weight: 500;
   line-height: 38px;
   letter-spacing: 0%;
-  text-align: left;
 `;
 
 export const Subtitle = styled.p`
   margin-top: 21px;
   color: rgb(75, 75, 75);
   font-family: 'Roboto', sans-serif;
-  font-size: 22px;
+  
   font-weight: 400;
   line-height: 26px;
   letter-spacing: 0%;
-  text-align: left;
+  text-align: center;
+
+  ${MOB} {
+    font-size: 16px;
+  }
+
+  ${TAB} {
+    font-size: 19px;
+  }
+
+  ${DESK1} {
+    font-size: 22px;
+  }
+
+  ${DESK2} {
+ 
+  }
 `;
